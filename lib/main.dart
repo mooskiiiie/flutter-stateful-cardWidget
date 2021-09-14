e5b5d9a64a0b5a56c64029c8e1d037446c8326f3
Sebastian <sebastianmvergara.tech@gmail.com>
2021-09-14 18:15:01 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import 'quote.dart';
+import 'quote_dart.dart';
+
+void main() => runApp(MaterialApp(
+      home: QuoteList(),
+    ));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(author: 'Sebastian Vergara', text: 'This is a quote'),
+    Quote(author: 'Oscar Wilson', text: 'This is another quote'),
+    Quote(author: 'Wilson Oscar', text: 'This is another quote'),
+    Quote(author: 'Rebecca Wilson', text: 'This is her quote')
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes
+            .map((quote) => QuoteCard(
+                quote: quote,
+                delete: () {
+                  setState(() {
+                    quotes.remove(quote);
+                  });
+                }))
+            .toList(),
+      ),
+    );
+  }
+}
